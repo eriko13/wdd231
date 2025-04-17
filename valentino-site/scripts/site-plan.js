28e0bb1d7578c24cc539f678e9675be7e4120e27
eriko13 <erikgerman917@gmail.com>
2025-04-16 22:30:38 -0700

    Valentinos World site plan

---
+++ valentino-site/scripts/site-plan.js
@@ -0,0 +1,5 @@
+// Get current year for copyright
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById('lastUpdated').textContent = document.lastModified; 
